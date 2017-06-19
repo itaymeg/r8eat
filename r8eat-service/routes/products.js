8378d065dc43943304d14f5f9c8b6a0ec855e5af
Itay Megera <itaymegera@gmail.com>
2017-06-19 17:18:44 +0300

    add products by id

--- r8eat-service/routes/products.js
+++ r8eat-service/routes/products.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET products listing. */
 router.get('/:id', function (req, res) {
 
     res.send('the id you gave me is ' + req.params.id);
